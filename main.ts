04baa2e33aa7360891c13a50f6e649005c177be6
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-10-04 15:53:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
-function enter () {
-	
-}
-function EnableChat () {
-    keyboard.key("q", KeyboardKeyEvent.Press)
-}
 pins.D10.digitalWrite(false)
 forever(function () {
     if (input.buttonD11.isPressed()) {
-        EnableChat()
-        pause(100)
         pins.D13.digitalWrite(true)
         keyboard.type("!yellow")
     } else {
